6a0ab127d6eb3bd921f53d4319fa1ffef544288c
Jessica <alfa_jess92@hotmail.com>
2020-05-19 16:40:38 +0200

    Add a scene with vuforia

---
+++ Assets/Scripts/PourDetector.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class PourDetector : MonoBehaviour
+{
+    public int pourThreshold = 45;
+    public Transform origin = null;
+    public GameObject streamPrefab = null;
+
+
+    private bool isPouring = false;
+    private Stream currentStream = null;
+
+
+    private void Update()
+    {
+        bool pourCheck = CalculatePourangle() < pourThreshold;
+
+        if (isPouring!= pourCheck)
+        {
+            isPouring = pourCheck;
+            if (isPouring)
+            {
+                StartPour();
+            }
+            else
+            {
+                EndPour();
+            }
+        }
+    }
+
+    private void StartPour()
+    {
+        //empty
+        print("Start");
+        currentStream = CreateStream();
+        currentStream.Begin();
+    }
+
+    private void EndPour()
+    {
+
+        currentStream.End();
+        currentStream = null;
+        print("End");
+
+    }
+
+
+    private float CalculatePourangle()
+    {
+        return transform.forward.y * Mathf.Rad2Deg;  //forward bcs of the mesh we are using, other can be up, video 2 min 7:40
+    }
+
+    private Stream CreateStream()
+    {
+        GameObject streamObject = Instantiate(streamPrefab, origin.position, Quaternion.identity, transform);//rotation of zero (quaternion)
+        return streamObject.GetComponent<Stream>();
+    }
+
+}
---
+++ Assets/Scripts/Stream.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stream : MonoBehaviour
+{
+    private LineRenderer lineRenderer = null;
+
+    private ParticleSystem splashParticle = null;
+
+    private Coroutine pourRoutine = null;
+    private Vector3 targetPosition = Vector3.zero;
+
+    private void Awake()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+        splashParticle = GetComponentInChildren<ParticleSystem>();
+    }
+
+    private void Start()
+    {
+        MoveToPosition(0, transform.position);
+        MoveToPosition(1, transform.position);
+    }
+
+    public void Begin ()
+    {
+        StartCoroutine(UpdateParticle());
+        pourRoutine=StartCoroutine(BeginPour());
+    }
+
+    private IEnumerator BeginPour()
+    {
+        while (gameObject.activeSelf)
+        {
+            targetPosition = FindEndPoint();
+
+            MoveToPosition(0, transform.position);
+            AnimateToPosition(1, targetPosition);
+            yield return null;
+        }
+        
+
+    }
+
+    public void End()
+    {
+        StopCoroutine(pourRoutine);
+        pourRoutine = StartCoroutine(EndPour());
+    }
+
+    private IEnumerator EndPour()
+    {
+        while (!HasReachedPosition(0, targetPosition)) //while not has reach this position, the animation is on
+        {
+            AnimateToPosition(0, targetPosition);
+            AnimateToPosition(1, targetPosition);
+            yield return null;//to avoid crash
+        }
+        Destroy(gameObject);// will be destroyed once the endpoint has left the origin of the bottle and hits the ground
+    }
+
+    private Vector3 FindEndPoint()
+    {
+        RaycastHit hit;
+        Ray ray = new Ray(transform.position, Vector3.down);
+
+        Physics.Raycast(ray, out hit, 2.0f);
+        Vector3 endPoint = hit.collider ? hit.point : ray.GetPoint(2.0f);// either the position we are hit or the end of the raycast
+
+        return endPoint;
+    }
+
+    private void MoveToPosition(int index, Vector3 targetPosition)
+    {
+        lineRenderer.SetPosition(index, targetPosition);
+    }
+
+    private void AnimateToPosition(int index, Vector3 targetPosition)
+    {
+        Vector3 currentPoint = lineRenderer.GetPosition(index);//the current position of the linerender 
+        Vector3 newPosition = Vector3.MoveTowards(currentPoint, targetPosition, Time.deltaTime * 1.75f); //if wants faster modify this parameter, movetowards is used as animation
+        lineRenderer.SetPosition(index, newPosition);//store the new position
+    }
+
+    private bool HasReachedPosition (int index, Vector3 targetPosition)
+    {
+        Vector3 currentPosition = lineRenderer.GetPosition(index);//getting the position of the index and evaluating if the position met the position we are trying to (target position)
+
+        return currentPosition==targetPosition;//return true or false
+    }
+
+    private IEnumerator UpdateParticle()
+    {
+
+        while (gameObject.activeSelf)
+        {
+            splashParticle.gameObject.transform.position = targetPosition;
+            //we want so show the particles only when the stream has reach the ground
+            bool isHitting = HasReachedPosition(1, targetPosition);
+            splashParticle.gameObject.SetActive(isHitting);
+            yield return null;
+        }
+        
+    }
+}
