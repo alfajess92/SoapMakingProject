98379b2a015d89e5718d606fee1752d1a351907d
Jessica <alfa_jess92@hotmail.com>
2020-05-23 18:27:16 +0200

    Added teapot animation and particle system

---
+++ Assets/Scripts/SpillScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpillScript : MonoBehaviour
+{
+    ParticleSystem waterStream;
+    // Start is called before the first frame update
+    void Start()
+    {
+        waterStream = GetComponent<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Angle(Vector3.down, transform.forward) <= 90f)
+        {
+            waterStream.Play();
+
+        }
+        else
+        {
+            waterStream.Stop();
+        }
+    }
+}
