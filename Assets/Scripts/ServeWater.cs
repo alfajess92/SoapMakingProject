2a5511a994201af2ab65a638b2813a6dc5d010ee
Jessica <alfa_jess92@hotmail.com>
2020-05-23 19:24:19 +0200

    Serve water raycasting and added sound of water being served into the ash tray please

---
+++ Assets/Scripts/ServeWater.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+using UnityEngine.Events;
+
+public class ServeWater : MonoBehaviour
+{
+
+
+
+    public GameObject definedButton;
+    public UnityEvent OnClick = new UnityEvent();
+    Animator animator;
+    Vector3 position;
+
+    //// Start is called before the first frame update
+    void Start()
+
+
+    {
+        definedButton = this.gameObject;
+        animator = GetComponentInParent<Animator>();
+
+        position = definedButton.transform.localPosition;
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit Hit;
+
+        if (Input.GetMouseButtonDown(0))//zero refers to the right click of the mouse
+        {
+            if (Physics.Raycast(ray, out Hit) && Hit.collider.gameObject == gameObject)
+            {
+
+                animator.SetTrigger("Serve");//inside the animator controller
+
+
+            }
+
+
+
+
+        }
+
+
+    }
+
+}
--- Assets/Scripts/SpillScript.cs
+++ Assets/Scripts/SpillScript.cs
@@ -5,23 +5,47 @@ using UnityEngine;
 public class SpillScript : MonoBehaviour
 {
     ParticleSystem waterStream;
+    Transform teapotTransform;
+    Vector3 position;
+    AudioSource audioSource;
+    public GameObject teapot;
     // Start is called before the first frame update
     void Start()
     {
         waterStream = GetComponent<ParticleSystem>();
+        teapot = GameObject.Find("Teapot");
+        teapotTransform = teapot.transform;
+        audioSource = GetComponent<AudioSource>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Vector3.Angle(Vector3.down, transform.forward) <= 90f)
+        //if (Vector3.Angle(Vector3.down, transform.forward) <= 90f)
+        //{
+        //    waterStream.Play();
+
+        //}
+        //else
+        //{
+        //    waterStream.Stop();
+        //}
+        position = teapotTransform.localPosition;
+        Debug.Log(position.y);
+        if (position.y >=0.15f)
         {
             waterStream.Play();
+            if (!audioSource.isPlaying)                    //if the audio is not playing
 
+            {
+                audioSource.Play();
+            }
         }
         else
         {
             waterStream.Stop();
+            audioSource.Stop();
         }
     }
 }
