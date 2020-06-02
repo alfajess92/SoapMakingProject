286de15092d4fbad55c81df813e4291b32f1d0d0
Jessica <alfa_jess92@hotmail.com>
2020-06-02 13:23:34 +0200

    Test extracting the time of the animation to called it inside the method

--- Assets/Scripts/TeapotScript.cs
+++ Assets/Scripts/TeapotScript.cs
@@ -14,6 +14,11 @@ public class TeapotScript : MonoBehaviour
     AudioSource audioSource;
     public bool isTouchTeapot = false;
 
+    AnimatorStateInfo animationState;
+    AnimatorClipInfo[] animatorClip;
+    float mytime;
+
+
     //// Start is called before the first frame update
     void Start()
 
@@ -22,26 +27,42 @@ public class TeapotScript : MonoBehaviour
         animator = GetComponent<Animator>();
         animatorWaterAsh = waterAsh.GetComponent<Animator>();
         audioSource = GetComponent<AudioSource>();
+
+
+        animationState = animator.GetCurrentAnimatorStateInfo(0);
+        animatorClip = animator.GetCurrentAnimatorClipInfo(0);
+        mytime = animatorClip[0].clip.length * animationState.normalizedTime;
+
+        Debug.Log(mytime);
     }
 
- 
+
     public void ServeWaterTeapot()
     {
         animator.SetTrigger("Serve");//inside the animator controller
         animatorWaterAsh.SetTrigger("Fill");
+        //isTouchTeapot = true;
+        Debug.Log(mytime);
+        Invoke("ReServeWaterTeapot", mytime);//
+    }
+
+
+    public void ReServeWaterTeapot()
+    {
         isTouchTeapot = true;
     }
+
 
     public void PlayWaterStream()//to add in the animator controller as a parameter
     {
         spill.PlayWaterStreamTeapot();
     }
 
+
     public void StopWaterStream()
     {
         spill.StopWaterStreamTeapot();
     }
-
 
 
     public void PlaySoundTeapotStream()//TODO check if this has to be here
