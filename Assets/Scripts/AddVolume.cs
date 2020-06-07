82fef7dbb7b92e93780e8a0eafec731eea8ecc2b
Jessica <alfa_jess92@hotmail.com>
2020-06-07 18:33:11 +0200

    change the add volume script to react using slider UI

--- Assets/Scripts/AddVolume.cs
+++ Assets/Scripts/AddVolume.cs
@@ -1,36 +1,75 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AddVolume : MonoBehaviour
 {
 
-    public float volumeincrease= 10f;
-    public GameObject liquidMesh;
+    //public float volumeincrease= 0.1f;
+    public GameObject liquidMesh, beaker;
     Transform liquidMeshTransform;
-    
+
+    //Vector3 changeVolume = new Vector3(0, 1f, 0);
     Vector3 currentVolume;
-    [SerializeField]
-    //Transform liquidMeshTransform;
-    // Start is called before the first frame update
-    void Start()
+
+    public Slider MySlider;
+
+    private void Start()
     {
 
-        liquidMeshTransform = GetComponentInChildren<Transform>();
-        currentVolume = liquidMeshTransform.localScale;
+        liquidMeshTransform= beaker.transform.Find("Liquid");
+        //if (liquidMesh != null)
+        //{
+        //    Debug.Log("A child with the name 'liquid' attached to the player");
+        //}
+
+        //currentVolume = liquidMesh.transform.localScale;
+        //liquidMeshTransform.localScale;
+        //print(currentVolume);
+        //changeVolume = new Vector3(1f, volumeincrease, 1f);
+
     }
 
-    ////Update is called once per frame
-    //void Update()
-    //{
-    //    AdjustVolume(volumeincrease);
-    //}
+    
 
-    public void AdjustVolume(float volumeincrease)
-    {
-        currentVolume.y += Time.deltaTime * volumeincrease;
-        //currentVolume.y += Time.deltaTime * volumeincrease;
 
-        liquidMeshTransform.localScale = currentVolume;
+
+        ////liquidMesh = GameObject.Find("Liquid");
+        
+
+        //liquidMeshTransform = GetComponentInChildren<Transform>();
+        //currentVolume = liquidMeshTransform.localScale;
+
+       
+    
+
+
+
+////Update is called once per frame
+//void Update()
+//{
+//    AdjustVolume();
+//}
+
+public void AdjustVolume()
+{
+        //changeVolume.y += Time.deltaTime * volumeincrease;
+        //currentVolume += changeVolume;
+        print("adding volume");
+
+
+        //currentVolume = Vector3.Scale(currentVolume, changeVolume);
+
+        //liquidMeshTransform.localScale = changeVolume;
+        //liquidMesh.transform.localScale +=changeVolume;
+
+        liquidMeshTransform.localScale = new Vector3(liquidMeshTransform.localScale.x, MySlider.value, liquidMeshTransform.localScale.z);
+
+
+
     }
+
+
+
 }
