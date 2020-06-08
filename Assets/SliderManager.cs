0dec0efa4e59dae462f7f256a45268bf69bff46a
Jessica <alfa_jess92@hotmail.com>
2020-06-08 11:50:56 +0200

    Add SliderManager to control the volume

---
+++ Assets/SliderManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderManager : MonoBehaviour
+{
+    public Slider slider;
+
+    float addedVolume;
+
+
+
+    public void selectVolume()
+    {
+        addedVolume = slider.value;
+        if (slider.value > 0.1f&& slider.value< 0.2f)
+        {
+
+            print(addedVolume);
+        }
+    }
+
+}
