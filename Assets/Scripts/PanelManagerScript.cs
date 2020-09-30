59614157903e6b8ad6e7e0007009b514985dac7e
Jessica <alfa_jess92@hotmail.com>
2020-09-30 11:09:19 +0200

    Add to the panel manager the gameobjects to be active/desactive when the nextpanel appears

--- Assets/Scripts/PanelManagerScript.cs
+++ Assets/Scripts/PanelManagerScript.cs
@@ -6,12 +6,22 @@ public class PanelManagerScript : MonoBehaviour
 {
 
     public Animator animator;
-    
+
+    //object to activate/deactivate when the panel is on/off
+    public GameObject table, resultbox;
+
+
+    private void Awake()
+    {
+        table = GameObject.Find("ResultsTable");
+        resultbox= GameObject.Find("ResultBox");
+    }
 
     public void EnterNextScenePanel()
     {
         animator.SetBool("isOn", true);
-        
+        table.SetActive(false);
+        resultbox.SetActive(false);
     }
 
     public void ExitNextScenePanel ()
@@ -19,5 +29,9 @@ public class PanelManagerScript : MonoBehaviour
     {
         //Debug.Log("end");
         animator.SetBool("isOn", false);
+        table.SetActive(true);
+        resultbox.SetActive(true);
     }
+
+   
 }
